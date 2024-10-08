958a5918204084cf701daf05eb5d3c0602ed2f12
abdelrhman1812 <abdelrhmanali1812@gmail.com>
2024-10-08 18:14:36 +0300

    uploda files

--- src/utils/hashAndCompare.js
+++ src/utils/hashAndCompare.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const hashPassword = ({ plaintext, salt = process.env.SALT_ROUND } = {}) => {
   const hashResult = bcrypt.hashSync(plaintext, parseInt(salt));
